2776ac8c3baa4949c5f33c353a4ccee3d338ae74
Wuliangsu <43233196+dalinuoer@users.noreply.github.com>
2020-04-24 13:59:57 +0800

    Update.

---
+++ exp04/exp04_1.m
@@ -0,0 +1,5 @@
+% Exp04-1
+% Author: Qin Hao
+% Date: 2020/4/24
+
+%------Test目录下有图像windmill_noise.png，用Matlab写程序，去除条纹干扰。------
---
+++ exp04/exp04_2.m
@@ -0,0 +1,5 @@
+% Exp04-2
+% Author: Qin Hao
+% Date: 2020/4/24
+
+%------Test目录下有图像boardWithNoise.jpg，用Matlab写程序，采用自适应中值滤波器去除噪声干扰。------
